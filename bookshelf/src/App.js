9046aad1a5b912b5cd95fff0af46718de3b66d1f
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-10 23:42:09 +0530

    add app file

---
+++ bookshelf/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Books from "./components/books";
+
+function App() {
+  return (
+    <main className="container">
+      <Books />
+    </main>
+  );
+}
+
+export default App;
