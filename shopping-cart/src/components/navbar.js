0c3461a18a1e2b09589f8b9af3bc998ac1a21b26
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-13 00:12:58 +0530

    add hamburger menu

--- shopping-cart/src/components/navbar.js
+++ shopping-cart/src/components/navbar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./nav.css";
 
 // Stateless Functional Component
-const NavBar = ({ totalCounters }) => {
+const NavBar = ({ totalCounters, onClick, trigger }) => {
+  let classes = "hamburger";
+  if (!trigger) classes += "-active";
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-light bg-light p-4">
+      <div onClick={onClick} className={classes}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
       <a className="navbar-brand" href="/">
         Cart{" "}
         <span className="badge badge-pill badge-secondary">
