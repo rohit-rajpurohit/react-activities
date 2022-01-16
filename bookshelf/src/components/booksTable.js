d4ec29e946368f804a1c37d24ac7c3bd9c1b237a
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-17 01:23:02 +0530

    refactoring (separate reusable table component)

--- bookshelf/src/components/booksTable.js
+++ bookshelf/src/components/booksTable.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Like from "./common/like";
-import TableHeader from "./tableHeader";
+import Table from "./table";
 
 class BooksTable extends React.Component {
   //local property columns (not the part of state as this will not change throughout the lifecycle of this component)
@@ -11,43 +11,35 @@ class BooksTable extends React.Component {
     { path: "pages", label: "Pages" },
     { path: "published", label: "Published" },
     { path: "ratings", label: "Ratings" },
-    { key: "like" },
-    { key: "delete" },
+    {
+      key: "like",
+      content: (book) => (
+        <Like liked={book.liked} onClick={() => this.props.onLike(book)} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (book) => (
+        <button
+          onClick={() => this.props.onDelete(book)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    },
   ];
 
   render() {
-    const { books, onDelete, onLike, onSort, sortColumn } = this.props;
+    console.log(this.props);
+    const { books, onSort, sortColumn } = this.props;
     return (
-      <table className="table table-dark table-striped">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <tbody>
-          {books.map((book) => (
-            <tr key={book.isbn}>
-              <td>{book.title}</td>
-              <td>{book.genre.name}</td>
-              <td>{book.author}</td>
-              <td>{book.pages}</td>
-              <td>{book.published}</td>
-              <td>{book.ratings}</td>
-              <td>
-                <Like onClick={() => onLike(book)} liked={book.liked} />
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(book)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Table
+        columns={this.columns}
+        data={books}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
