615691cbf646d9c3bb18fd2f5ec86b620db4d695
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-18 21:52:10 +0530

    add navbar component

---
+++ bookshelf/src/components/bookForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BookForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>BookForm-{match.params.isbn}</h1>
+      <button className="btn-primary" onClick={() => history.push("/")}>
+        Save
+      </button>
+    </div>
+  );
+};
+
+export default BookForm;
