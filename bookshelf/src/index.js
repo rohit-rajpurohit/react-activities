9626b7778b36a7cf3c01c495b8d2cb94efcf13b8
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-18 21:44:28 +0530

    add  BrowserRouter

--- bookshelf/src/index.js
+++ bookshelf/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,7 +8,9 @@ import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
